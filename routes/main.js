212c012a900aa0661e32036816e5091358edb058
SHTOA <howtomatben@gmail.com>
2023-12-13 12:13:41 +0000

    reroutes

--- routes/main.js
+++ routes/main.js
@@ -394,13 +394,13 @@ app.post("/topics/:topic/join", function(req,res,next){
 
                 db.query(membershipQuery, (err, result) => {
                     if (err) {
-                        res.redirect(`/`);
+                        res.redirect(`../topics/${req.params.topic}/posts`);
                         console.log(err)
                     } else {
                         //console.log(req.session)
                         req.session.memberships = result[0];
                         console.log(req.session.memberships);
-                        res.redirect(`/`);
+                        res.redirect(`../topics/${req.params.topic}/posts`);
 
                     }
                 });
